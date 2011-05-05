fda65da8368a528127acf3418756d4944df11324
Martin Nycander <mnyc@kth.se>
2011-05-05 14:02:40 +0200

    Fuck yes.

---
+++ program/brk.h
@@ -0,0 +1,7 @@
+#ifndef _brk_h_
+#define _brk_h_
+
+extern int brk(void *);
+extern void *sbrk(int );
+
+#endif /* _brk_h */
---
+++ program/tst.h
@@ -0,0 +1,13 @@
+
+#ifndef _tst_h_
+#define _tst_h_
+
+#ifndef MESSAGE
+#define MESSAGE(foo) fprintf(stderr,"%s, line %d: %s", progname, __LINE__, foo)
+#endif
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#endif
